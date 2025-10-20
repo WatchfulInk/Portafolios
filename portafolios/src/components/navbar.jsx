cd5f03dc85a4226c0998ee132798e5680709a585
WatchfulInk <alan-bella@hotmail.com>
2025-10-20 17:54:22 -0600

    feature/navbar_base

---
+++ portafolios/src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/Aboutme">About me</a></li>
+        <li><a href="/Projects">Projects</a></li>
+        <li><a href="/Knowledge">Knowledge</a></li>
+        <li><a href="/Experience">Experience</a></li>
+        <li><a href="/Certifications">Certifications</a></li>
+        <li><a href="/Contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
--- portafolios/src/pages/homepage.jsx
+++ portafolios/src/pages/homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Navbar from '../components/navbar';
 
 function Homepage() {
 
   return (
     <div>
+      <Navbar />
       <h1>Inicio</h1>
     </div>
   )
