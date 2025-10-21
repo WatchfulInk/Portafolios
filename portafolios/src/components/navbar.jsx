69f461debd0a64dceb6338625b38856a7501e049
WatchfulInk <alan-bella@hotmail.com>
2025-10-20 19:37:19 -0600

    add_navbar_test

--- portafolios/src/components/navbar.jsx
+++ portafolios/src/components/navbar.jsx
@@ -1,16 +1,35 @@
+import clsx from "clsx";
 import React from "react";
+
+// Navigation links for the navbar
+const navigation = [
+  { name: 'Home', href: '/' , current: true },
+  { name: 'About me', href: '/Aboutme', current: false },
+  { name: 'Projects', href: '/Projects', current: false },
+  { name: 'Knowledge', href: '/Knowledge', current: false },
+  { name: 'Experience', href: '/Experience', current: true },
+  { name: 'Certifications', href: '/Certifications', current: false },
+  { name: 'Contact', href: '/Contact', current: false },
+];
+
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/Aboutme">About me</a></li>
-        <li><a href="/Projects">Projects</a></li>
-        <li><a href="/Knowledge">Knowledge</a></li>
-        <li><a href="/Experience">Experience</a></li>
-        <li><a href="/Certifications">Certifications</a></li>
-        <li><a href="/Contact">Contact</a></li>
+    <nav className="bg-gray-800 p-4">
+      <ul className="flex space-x-4">
+        {navigation.map((item) => (
+          <li key={item.name}>
+            <a 
+            href={item.href} 
+            className={clsx("hover:text-gray-400", {
+                'font-semibold underline': item.current,
+                'bg-white': !item.current
+              })}
+            >
+              {item.name}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
